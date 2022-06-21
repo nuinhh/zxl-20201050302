1a597cec17c6875e21042444ad6c4898e802f49e
nuinhh <102286754+nuinhh@users.noreply.github.com>
2022-06-21 23:51:47 +0800

    Update work_3_2.cpp

--- 郑晓雷_20201050302/实验3/work_3_2.cpp
+++ 郑晓雷_20201050302/实验3/work_3_2.cpp
@@ -1,64 +1,57 @@
 #include <windows.h>
 #include <GL/glut.h>
-#include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
 
-void setPixel(int x, int y)
-{
-    glPointSize(5.0f);
-    glBegin(GL_POINTS);
-    glVertex2i(x, y);
-    glEnd();
-    glFlush();
-}
-
 void init(void)
 {
-    glClearColor(1.0f, 1.0f, 1.0f, 0.0f); //设置显示窗口颜色为白色
-    glMatrixMode(GL_PROJECTION);
-    gluOrtho2D(0.0f, 200.0f, 0.0f, 150.0f); //设置规划参数
+    glClearColor (0.5, 1.0, 1.0, 0.0);  // 指定清空颜色（背景色）为白色
+    gluOrtho2D (-150.0, 150.0, -150.0, 150.0);   //指定二维坐标系中被显示的区域
 }
-void proBresenhamLine(int x0, int y0, int x1, int y1)
-{
-    int dx, dy, e, x, y;
-    dx = x1 - x0;
-    dy = y1 - y0;
-    e = -dx;
-    x = x0;
-    y = y0;
-    while (x <= x1)
-    {
-        setPixel(x, y);
-        x++;
-        e = e + 2 * dy;
-        if (e > 0)
-        {
-            y++;
-            e = e - 2 * dx;
+
+void setPixel(int x,int y){
+	glPointSize(1.0f);
+	glBegin(GL_POINTS);
+		glVertex2i(x,y);
+	glEnd();
+	glFlush();
+}
+
+void myBresehamPro(int x0,int y0,int xEnd,int yEnd){
+    int dx=xEnd-x0,dy=yEnd-y0;
+    int x=x0,y=y0;
+    int e=-dx;
+    setPixel(x,y);
+    for(x;x<xEnd;x++){
+        e=e+2*dy;
+        if(e>0){
+        x+=1;
+        y+=1;
+        e-=2*dx;
+        setPixel(x,y);
+        }
+        else{
+        x+=1;
+        setPixel(x,y);
         }
     }
 }
 
-void myDisplay()
-{
-    int x0, y0, xEnd, yEnd;
-    //s输入起点中点位置
-    scanf_s("%d %d %d %d", &x0, &y0, &xEnd, &yEnd);
-    proBresenhamLine(x0, y0, xEnd, yEnd);
+void myDisplay(void){
+    glClear(GL_COLOR_BUFFER_BIT);// 清空显示窗口
+    glColor3f(0.0,0.0,0.0);// 指定前景色（当前绘制颜色）为黑色
+    myBresehamPro(0,0,100,30);
+    glFlush();// 使绘制立即反映到屏幕上
 }
 
-void main(int argc, char **argv)
+int main (int argc, char** argv)
 {
-    glutInit(&argc, argv);
-    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB); //初始化显示模式
-    glutInitWindowSize(600, 600);       //设置窗口尺寸
-    glutInitWindowPosition(200, 100);     //设置窗口位置
-    glutCreateWindow("Bresenham算法");  //创建窗口
-    glutDisplayFunc(&myDisplay);         //注册显示函数
-    glClearColor(1.0f, 1.0f, 1.0f, 1.0f);    //设置背景颜色
-    glColor3f(1.0f, 0.0f, 0.0f);           //设置绘制颜色
-    glMatrixMode(GL_PROJECTION);   
-    gluOrtho2D(0.0, 600, 0.0, 600);       //设置投影区域
-    glutMainLoop();                   //进入程序循环
+    glutInit (&argc, argv);                         // 初始 GLUT.
+    glutInitDisplayMode (GLUT_SINGLE | GLUT_RGB);   //设定显示模式
+    glutInitWindowPosition (500, 200);   // 设定窗口位置
+    glutInitWindowSize (300, 300);      // 设定窗口大小
+    glutCreateWindow ("Bresenhamplus");
+    init( );                       // 进行一些初始化工作
+    glutDisplayFunc(&myDisplay);     // 指定绘制的回调函数
+    glutMainLoop ( );          // 进入无穷循环，等待事件处理
 }
